f3632932c7a26463953e3f390e8eadcb21d813e9
Unknown <bdaniel-ctr@iwsinc.com>
2018-08-24 12:13:25 -0700

    add framework

---
+++ Example/KWDrawer/KWDrawer.h
@@ -0,0 +1,19 @@
+//
+//  KWDrawer.h
+//  KWDrawer
+//
+//  Created by Bruce on 8/24/18.
+//  Copyright © 2018 Kawoou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KWDrawer.
+FOUNDATION_EXPORT double KWDrawerVersionNumber;
+
+//! Project version string for KWDrawer.
+FOUNDATION_EXPORT const unsigned char KWDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KWDrawer/PublicHeader.h>
+
+
